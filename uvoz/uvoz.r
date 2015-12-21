28f4f6aedd3555a6ad715cd9e857b2016feeb2a7
Kaja Vezenšek <kaja.vezensek@gmail.com>
2015-12-21 19:47:59 +0100

    Razširjeni podatki

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,15 +1,15 @@
 library(dplyr)
 
-# Funkcija, ki uvozi podatke iz datoteke place_surs.csv
-uvozi.place_surs<- function() {
-  return(read.csv2("podatki/place_surs.csv", sep = ";", as.is = TRUE,
+# Funkcija, ki uvozi podatke iz datoteke place_obcine.csv
+uvozi.place_obcine<- function() {
+  return(read.csv2("podatki/place_obcine.csv", sep = ";", as.is = TRUE,
                       skip=6, header=FALSE,
                       col.names = c("", "kraj", "mesec","bruto", "neto"),
                       fileEncoding = "windows-1250"))
 }
 
 # Zapišimo podatke v razpredelnico place.
-place <- uvozi.place_surs()
+place <- uvozi.place_obcine()
 
-place.kraj <- lapply(seq(1, nrow(place), 130), function(x) place[x + (1:129), c(-1, -2)])
-names(place.kraj) <- place[seq(1, nrow(place), 130), 2]
+place.kraj <- lapply(seq(1, nrow(place), 17), function(x) place[x + (1:16), c(-1, -2)])
+names(place.kraj) <- place[seq(1, nrow(place), 17), 2]
