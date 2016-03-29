7c32007ffbceec54f1b06293f7a01f4cac8ef730
Kaja Vezenšek <kaja.vezensek@gmail.com>
2016-03-29 21:56:11 +0200

    Začetek analize

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,4 +1,10 @@
 # 4. faza: Analiza podatkov
 
-barve <- rainbow(length(levels(obcine[[7]])))
-names(barve) <- levels(obcine[[7]])
+#Izriše graf na katerem so napovedi do leta 2026 bruto plač
+ggplot(data=zdr[['bruto']], aes(x=datum, y=bruto))+ xlim(2007, 2026) +
+  geom_line(size=0.5)+
+  geom_point(size=3, fill="black")+
+  ggtitle("Napoved rasti bruto plač do leta 2026")+
+  geom_smooth(method = "lm",formula = y ~ x+I(x^2)+I(x^3),
+              size = 1, fullrange = TRUE)
+
