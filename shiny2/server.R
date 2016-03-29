d00359897e46f6f146b8ae4415b5876f3e295a97
Kaja Vezenšek <kaja.vezensek@gmail.com>
2016-03-29 21:42:44 +0200

    popravljen shiny2

--- shiny2/server.R
+++ shiny2/server.R
@@ -2,7 +2,10 @@ library(shiny)
 
 shinyServer(function(input, output) {
   output$Graf <- renderPlot({
-    ggplot(zdr %>% filter(leto == input$izberi, bruto/povrsina > 50),
+    zdr.leto <- zdr %>% filter(leto == input$izberi)
+    ggplot(zdr.leto %>% group_by(kraj) %>%
+             summarise(razmerje = max(bruto/povrsina)) %>%
+             filter(razmerje > 50) %>% inner_join(zdr.leto, by = "kraj"),
            aes_string(x = "kraj", y = input$izb, fill = "mesec")) +
       geom_bar(stat="identity",position = "dodge",size=10) +
       coord_flip()
