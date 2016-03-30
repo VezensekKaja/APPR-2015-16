36ef06bd97a6c232b6e151a2946f69615068c17e
Kaja Vezenšek <kaja.vezensek@gmail.com>
2016-03-30 21:13:46 +0200

    vejica

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -4,8 +4,8 @@ zdr <- zdr %>% mutate(datum = paste(1, substr(mesec, 1, 3), leto) %>%
   mutate(timestamp = as.numeric(datum))
 
 #Izriše graf na katerem so napovedi do leta 2021 bruto plač za Ljubljano
-g1 <- ggplot(zdr %>% filter(kraj == "Ljubljana") %>% as.Date()),
-       aes(x=datum, y=bruto)) +
+g1 <- ggplot(zdr %>% filter(kraj == "Ljubljana"),
+             aes(x=datum, y=bruto)) +
   xlim(as.Date("2007-01-01"), as.Date("2021-01-01")) +
   geom_line(size=0.5)+
   geom_point(size=3, fill="black")+
@@ -18,13 +18,11 @@ lin <- lm(data = zdr, bruto ~ timestamp+I(timestamp^2)+I(log(timestamp)))
 #Izpiše višino plač od leta 2015 do leta 2021
 napoved <- predict(lin, data.frame(timestamp = paste0(c(2015:2021), "-1-1") %>%
                           as.Date() %>% as.numeric()))
-setNames(napoved, c(2015:2021))
+napoved <- setNames(napoved, c(2015:2021))
 
 
 #Izriše graf na katerem so napovedi do leta 2021 bruto plač na površino za Cerklje na Gorenjskem
-g2 <- ggplot(zdr %>% filter(kraj == "Cerklje na Gorenjskem") %>%
-         mutate(datum = paste(1, substr(mesec, 1, 3), leto) %>%
-                  strptime("%d %b %Y") %>% as.Date()),
+g2 <- ggplot(zdr %>% filter(kraj == "Cerklje na Gorenjskem"),
        aes(x=datum, y=bruto/povrsina)) +
   xlim(as.Date("2007-01-01"), as.Date("2021-01-01")) +
   geom_line(size=0.5)+
@@ -38,4 +36,4 @@ lin2 <- lm(data = zdr, bruto/povrsina ~ timestamp+I(log(timestamp)))
 #Izpiše višino razmerja bruto/površino od leta 2015 do leta 2021
 napoved2 <- predict(lin2, data.frame(timestamp = paste0(c(2015:2021), "-1-1") %>%
                           as.Date() %>% as.numeric()))
-setNames(napoved2, c(2015:2021))
+napoved2 <- setNames(napoved2, c(2015:2021))
