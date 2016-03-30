1d219c083f45ef9ffe7704cfe970c1e94d63aa29
Kaja Vezenšek <kaja.vezensek@gmail.com>
2016-03-30 20:11:35 +0200

    dopolnitev

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,7 +1,10 @@
 # 4. faza: Analiza podatkov
+zdr <- zdr %>% mutate(datum = paste(1, substr(mesec, 1, 3), leto) %>%
+                        strptime("%d %b %Y") %>% as.Date()) %>%
+  mutate(timestamp = as.numeric(datum))
 
 #Izriše graf na katerem so napovedi do leta 2021 bruto plač za Ljubljano
-ggplot(zdr %>% filter(kraj == "Ljubljana") %>%
+g1 <- ggplot(zdr %>% filter(kraj == "Ljubljana") %>%
          mutate(datum = paste(1, substr(mesec, 1, 3), leto) %>%
                   strptime("%d %b %Y") %>% as.Date()),
        aes(x=datum, y=bruto)) +
@@ -12,18 +15,18 @@ ggplot(zdr %>% filter(kraj == "Ljubljana") %>%
   geom_smooth(method = "lm",formula = y ~ x+I(x^(2))+I(log(x)), size = 1, fullrange = TRUE)
 
 #Izpiše koeficient, prosti člen
-lin <- lm(leto ~ bruto, data = zdr)
+lin <- lm(data = zdr, bruto ~ timestamp+I(timestamp^2)+I(log(timestamp)))
 
 #Izpiše višino plač od leta 2015 do leta 2021
-predict(lin, data.frame(leto = c(2015:2021)))
-
+napoved <- predict(lin, data.frame(timestamp = paste0(c(2015:2021), "-1-1") %>%
+                          as.Date() %>% as.numeric()))
 
 
 #Izriše graf na katerem so napovedi do leta 2021 bruto plač na površino za Cerklje na Gorenjskem
-ggplot(zdr %>% filter(kraj == "Cerklje na Gorenjskem") %>%
+g2 <- ggplot(zdr %>% filter(kraj == "Cerklje na Gorenjskem") %>%
          mutate(datum = paste(1, substr(mesec, 1, 3), leto) %>%
                   strptime("%d %b %Y") %>% as.Date()),
-       aes(x=datum, y=bruto/površina)) +
+       aes(x=datum, y=bruto/povrsina)) +
   xlim(as.Date("2007-01-01"), as.Date("2021-01-01")) +
   geom_line(size=0.5)+
   geom_point(size=3, fill="black")+
@@ -31,7 +34,8 @@ ggplot(zdr %>% filter(kraj == "Cerklje na Gorenjskem") %>%
   geom_smooth(method = "lm",formula = y ~ x+I(log(x)), size = 1, fullrange = TRUE)
 
 #Izpiše koeficient, prosti člen
-lin2 <- lm(leto ~ bruto/površino, data = zdr)
+lin2 <- lm(data = zdr, bruto/povrsina ~ timestamp+I(log(timestamp)))
 
-#Izpiše višino plač od leta 2015 do leta 2021
-predict(lin2, data.frame(leto = c(2015:2021)))
+#Izpiše višino razmerja bruto/površino od leta 2015 do leta 2021
+napoved2 <- predict(lin2, data.frame(timestamp = paste0(c(2015:2021), "-1-1") %>%
+                          as.Date() %>% as.numeric()))
